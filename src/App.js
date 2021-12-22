c82d2380c7c6d68f639ea14eacf5a505b5a21b6f
arnab pratim saha <arnabpratimsaha@gmail.com>
2021-12-22 19:33:10 +0530

    started

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <button onClick={()=>{window.location='http://localhost:5000/auth'}}>Google</button>
     </div>
   );
 }
