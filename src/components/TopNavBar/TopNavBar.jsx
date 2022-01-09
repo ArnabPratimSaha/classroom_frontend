339094cd91e17ecc04c09188df1350bea06c730c
EliteSushanta <SushantaSaren2000@gmail.com>
2022-01-10 00:59:32 +0530

    create class

--- src/components/TopNavBar/TopNavBar.jsx
+++ src/components/TopNavBar/TopNavBar.jsx
@@ -6,17 +6,39 @@ import { MdRssFeed } from "react-icons/md";
 import { BsPen, BsPeople } from "react-icons/bs";
 import Avatar from "../Avatar/Avatar";
 import DropDownDiv from "./components/DropDownDiv";
-import CustomInput from "../CustomInput/CustomInput";
+import CreateClassForm from "./components/CreateClassForm";
 
 
 const TopNavBar = () => {
     const dropDownRef = useRef();
+    const createClassFormRef = useRef();
+    const modalRef = useRef();
 
     const addOrJoinClassButtonClickHandler = () => {
         if (dropDownRef && dropDownRef.current) {
             dropDownRef.current.classList.toggle("open-drop-down__div");
         }
     };
+
+    const createClassButtonClickHandler = () => {
+        if(modalRef && modalRef.current){
+            modalRef.current.style.display = 'block';
+        }
+
+        if(createClassFormRef && createClassFormRef.current){
+            createClassFormRef.current.style.display = 'block'
+        }
+    }
+
+    const modalClickHandler = () => {
+        if(modalRef && modalRef.current){
+            modalRef.current.style.display = 'none';
+        }
+
+        if(createClassFormRef && createClassFormRef.current){
+            createClassFormRef.current.style.display = 'none'
+        }
+    }
 
     return (
         <>
@@ -62,54 +84,16 @@ const TopNavBar = () => {
                     </div> */}
                     <DropDownDiv
                         ref={dropDownRef}
-                        itemArray={[<span key={1}>Join Class</span>, <span key={2}>Create Class</span>]}
+                        itemArray={[<span key={1}>Join Class</span>, <span onClick={() => {createClassButtonClickHandler()}} key={2}>Create Class</span>]}
                     />
                 </div>
                 <Avatar height="3.5rem" width="3.5rem" />
             </div>
         </div>
-        <div className="top-navbar__modal"></div>
-        <div className="top-navbar__create-class__div">
-            <div className="top-navbar__create-class__inner-div">
-                <div className="create-class__carousel-item__div top-navbar__create-class__terms_conditions__div">
-                    <span className="top-navbar__create-class__terms_conditions__header">Terms & Conditions :</span>
-                    <br/>
-                    <br/>
-                    <span className="top-navbar__create-class__terms_conditions">
-                        1. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur.
-                    </span>
-                    <br/>
-                    <br/>
-                    <span className="top-navbar__create-class__terms_conditions">
-                        2. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur.
-                    </span>
-                    <br/>
-                    <br/>
-                    <span className="top-navbar__create-class__terms_conditions">
-                        3. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur.
-                    </span>
-                    <br/>
-                    <br/>
-                    <span className="top-navbar__create-class__terms_conditions">
-                        4. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur.
-                    </span>
-                    <br/>
-                    <br/>
-                    <div className="underline"></div>
-                    <br/>
-                    <div className="top-navbar__create-class__terms_conditions__input-div">
-                        <input type = 'checkbox'/>
-                        <label>Accept Terms & Conditions</label>
-                    </div>
-                    <button className="top-navbar__create-class__next-button" >next</button>
-                </div>
-                <div className="create-class__carousel-item__div create-class__info-div">
-                    <CustomInput/>
-                </div>
-                <div className="create-class__carousel-item__div"></div>
-                <div className="create-class__carousel-item__div"></div>
-            </div>
-        </div>
+            <div ref = {modalRef} onClick={() => {modalClickHandler()}} className="top-navbar__modal"></div>
+            <CreateClassForm
+                ref = {createClassFormRef}
+            />
         </>
     );
 };
