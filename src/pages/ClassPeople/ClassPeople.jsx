5d1a95b7ca94b1a9329f010a4d5c46afa65dacb0
EliteSushanta <SushantaSaren2000@gmail.com>
2022-01-29 00:33:27 +0530

    added people

--- src/pages/ClassPeople/ClassPeople.jsx
+++ src/pages/ClassPeople/ClassPeople.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { classPage } from "../../actions/actions";
 import './ClassPeople.css'
+import ClassPeopleCard from "./Components/ClassPeopleCard";
 
 const ClassPeople = () => {
 
@@ -20,7 +21,43 @@ const ClassPeople = () => {
         <>
             <div className="class__people__full-div">
                 <div className="class__people__inner-div">
-                    
+                    <div className="class-people__admin-div">
+                        <span className="class-people__role-header">Admin</span>
+                        <div style = {{marginBottom : '10px'}} className="underline"></div>
+                        <ClassPeopleCard
+                            name = "Sushanta"
+                        />
+                    </div>
+                    <div className="class-people__teachers-div">
+                        <span className="class-people__role-header">Teachers</span>
+                        <div style = {{marginBottom : '10px'}} className="underline"></div>
+                        <ClassPeopleCard
+                            name = "Sushanta"
+                        />
+                        <ClassPeopleCard
+                            name = "Sushanta"
+                        />
+                    </div>
+                    <div className="class-people__students-div">
+                        <span className="class-people__role-header">Students</span>
+                        <div style = {{marginBottom : '10px'}} className="underline"></div>
+                        <ClassPeopleCard
+                            name = "Sushanta"
+                            rollNo = "19CS8023"
+                        />
+                        <ClassPeopleCard
+                            name = "Sushanta"
+                            rollNo = "19CS8023"
+                        />
+                        <ClassPeopleCard
+                            name = "Sushanta"
+                            rollNo = "19CS8023"
+                        />
+                        <ClassPeopleCard
+                            name = "Sushanta"
+                            rollNo = "19CS8023"
+                        />
+                    </div>
                 </div>
             </div>
         </>
